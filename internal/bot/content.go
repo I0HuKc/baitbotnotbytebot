f5cf79324535ce0bd98b8ef3a2ebe83fc8775c01
I0HuKc <web-disain@mail.ua>
2022-05-01 09:00:40 +0300

    Убрал лишнее, добавил Действия

--- internal/bot/content.go
+++ internal/bot/content.go
@@ -1,30 +1,20 @@
 package baitbot
 
+// Actions
+var (
+	ActionSendJoke = CreateAction("SEND_JOKE")
+)
+
 // Commands
 var (
 	CommandStart              = CreateCommand("start")
-	CommandHelp               = CreateCommand("help")
 	CommandAddDecription      = CreateCommand("ad", "-v")
 	CommandGetDecription      = CreateCommand("gd")
 	CommandGetDescriptionList = CreateCommand("gdl")
+	CommandSendJoke           = CreateCommand("sjoke")
 
 	CommandAntre                 = CreateCommand("antre")
-	CommandStopAntre             = CreateCommand("sa")
 	CommandJoke                  = CreateCommand("joke")
 	CommandPing                  = CreateCommand("ping")
 	CommandStartChangeDecription = CreateCommand("scd")
 )
-
-var helpInfo string = `
-/start — перезапуск бота (локальный).
-
-*Групповые команды*
-/scd — старт процесса смены описания
-/bll — начать буллить человека
-/cd — принудительное изменение статуса в группе
-
-*Приватные команды*
-/ad -v <value> — добавить статус (for Authors)
-/gd -id <recordid> — получить статус (for Admins).
-/help — получить эту инструкцию.
-`
